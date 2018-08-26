987c5a8e3524fb05ae48ba12a12242243bb9804f
unknown <victorha@gmail.com>
2018-08-26 12:23:12 -0400

    fixed connection variable

--- config/connection.js
+++ config/connection.js
@@ -9,10 +9,10 @@ function con() {
     //     database : 'burgers_db'
     // });
     if (process.env.JAWSDB_URL) {
-        connection = mysql.createConnection(process.env.JAWSDB_URL);
+        return mysql.createConnection(process.env.JAWSDB_URL);
     }
     else {
-        connection = mysql.createConnection({
+        return mysql.createConnection({
             host     : 'localhost',
             port: 3306,
             user     : 'root',
